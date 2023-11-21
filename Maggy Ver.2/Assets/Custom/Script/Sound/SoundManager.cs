a3b6c78540e509fe63150cb815d708c2f88f0e62
somebody54569 <sorawis.leep@bumail.net>
2023-11-21 23:00:42 +0700

    Sound Manager in

---
+++ Maggy Ver.2/Assets/Custom/Script/Sound/SoundManager.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager _instance;
+    public static SoundManager instance => _instance;
+
+    public static bool hasIntance => _instance != null;
+    public enum SoundName
+    {
+        PlayrBites,
+        HitPlayer,
+        CollectItem
+    }
+
+    [SerializeField] private Sound[] _sounds;
+
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Play(SoundName name)
+    {
+        Sound sound = GetSound(name);
+        if (sound.audioSource == null)
+        {
+            sound.audioSource = gameObject.AddComponent<AudioSource>();
+            sound.audioSource.clip = sound.clip;
+            sound.audioSource.volume = sound.volume;
+            sound.audioSource.loop = sound.loop;
+        }
+
+        sound.audioSource.Play();
+    }
+
+    private Sound GetSound(SoundName name)
+    {
+        return Array.Find(_sounds, s => s.soundName == name);
+    }
+}
